a6d57d939760ed3c6b7f6c45a0602c7f37c553e5
Adithya <53051177+Pro-Grammar05@users.noreply.github.com>
2021-04-10 20:16:39 +0530

    Update live_ipl_score.py

--- live_ipl_score.py
+++ live_ipl_score.py
@@ -3,26 +3,26 @@ import requests
 import sys
 import time
 
-r = requests.get('https://www.cricbuzz.com/cricket-match/live-scores')
-soup = bs(r.content, features = 'html.parser')
+while True:
+    r = requests.get('https://www.cricbuzz.com/cricket-match/live-scores')
+    soup = bs(r.content, features = 'html.parser')
 
-try:
-    div = soup.find("div", attrs = {"ng-show" : "active_match_type == 'league-tab'"})
-    
-    title = div.find("h2").text
-
-    if title != "INDIAN PREMIER LEAGUE 2021":
-        print("\nNo IPL matches at the moment")
-        sys.exit()
+    try:
+        div = soup.find("div", attrs = {"ng-show" : "active_match_type == 'league-tab'"})
         
-    print(f'\n{title}', end='\n\n')
+        title = div.find("h2").text
 
-    while True:
+        if title != "INDIAN PREMIER LEAGUE 2021":
+            print("\nNo IPL matches at the moment")
+            sys.exit()
+            
+        print(f'\n{title}', end='\n\n')
+
         matches = div.find_all(class_ = "cb-mtch-lst cb-col cb-col-100 cb-tms-itm")
         for match in matches:
             team_names = match.find("h3")
             print(team_names.text[1:-2], end='\n\n')
-            
+                
             teams = match.find_all(class_ = "cb-ovr-flo cb-hmscg-tm-nm")
             team_scores = match.find_all("div", attrs = {"style" : "display:inline-block; width:140px"})
 
@@ -32,7 +32,7 @@ try:
             live = match.find(class_ = "cb-text-live")
             print('\n' + live.text)
 
-            time.sleep(45)
+    except AttributeError:
+        print("\nNo IPL matches at the moment")
 
-except AttributeError:
-    print("\nNo IPL matches at the moment")
+    time.sleep(45)
